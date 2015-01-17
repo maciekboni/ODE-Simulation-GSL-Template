b8da54ae1adc95ea82ff11a8b501edb414ecd396
maciekboni <maciekboni@gmail.com>
2015-01-18 02:52:48 +0700

    Create predict.h

---
+++ predict.h
@@ -0,0 +1,10 @@
+#include "essentials.h"
+
+// this function integrates from time t0 to time t1
+void predict( double t0, double t1, double* y0, PARAMS* pp, double* incidence,
+		 gsl_odeiv_step* s, gsl_odeiv_control* c, gsl_odeiv_evolve* e);
+		 
+
+
+
+
